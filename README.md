15a14203db746ee763d573d3e6a61280a42cfada
Jean Lucas <jeanleonino@gmail.com>
2016-02-25 04:13:37 -0400

    First commit
    Solves #2 #5 #6 and #7

---
+++ README.md
@@ -0,0 +1,20 @@
+# Boilerplate for projects using Gulp + Backbone + Marionette
+This project is the starting point, you can just fork it and adapt to your own
+set up. What this project uses:
+
+- Gulp
+- Bower to manage versions
+- scss for styles
+- UIKit for basic bootstrapping (optional)
+
+## Install
+Before installing it is recommended to check if you have these on your environment:
+
+- Node.js (recommended [nvm](https://github.com/creationix/nvm)) and npm
+- [Gulp](http://gulpjs.com/)
+- [Bower](http://bower.io/)
+- [Karma](https://github.com/karma-runner/karma)
+
+Run `npm install` and `bower install` after cloning the project.
+
+## Contributing
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var gutil = require('gulp-util');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var notify = require("gulp-notify");
+var plumber = require('gulp-plumber');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var livereload = require('gulp-livereload');
+
+
+gulp.task('lint', function() {
+  // place code for your default task here
+});
+
+gulp.task('uglify', function() {
+  // place code for your default task here
+});
+
+gulp.task('sass', function() {
+  // place code for your default task here
+});
+
+gulp.task('dist', function() {
+  // place code for your default task here
+});
+
+gulp.task('watch', function() {
+  // place code for your default task here
+});
+
+gulp.task('server', function() {
+  // place code for your default task here
+});
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
